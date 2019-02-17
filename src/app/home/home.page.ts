acdf013654be092a17ab971e7e1d0cf49a5ed8c0
Sanderhelleso <sanderhelleso@hotmail.com>
2019-02-16 21:44:04 -0800

    added total timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,9 +11,16 @@ export class HomePage {
 	radius: number = 100;
 	fullTime: any = '00:01:30';
 	timer: any = false;
+	overallTimer: any = false;
 	progress: any = 0;
 	minutes: number = 1;
 	seconds: any = 30;
+
+	elapsed: any = {
+		h: '00',
+		m: '00',
+		s: '00'
+	};
 
 	// initialize timer
 	startTimer() {
@@ -38,13 +45,41 @@ export class HomePage {
 
 			console.log(this.percent);
 		}, 1000);
+	}
 
+	progressTimer() {
+
+		const countDownDate = new Date();
+
+		this.overallTimer = setInterval(() => {
+			const now = new Date().getTime();
+			const distance = now - countDownDate.getTime();
+
+			// time calculations for hours, minutes and seconds
+			this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+			// add leading 0
+			this.elapsed.h = this.pad(this.elapsed.h, 2);
+			this.elapsed.m = this.pad(this.elapsed.m, 2);
+			this.elapsed.s = this.pad(this.elapsed.s, 2);
+		}, 1000);
+	}
+
+	// add leading 0 to timer
+	pad(num, size) {
+
+		let s = `${num}`;
+		while (s.length < size) s = `0${s}`;
+		return s;
 	}
 
 	// reset timer
 	resetTimer() {
 
 		if (this.timer) clearInterval(this.timer);
+		if (!this.overallTimer) this.progressTimer();
 
 		this.timer = false;
 		this.percent = 0;
